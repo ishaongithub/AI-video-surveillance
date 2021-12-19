8ca1ebff40d200cb0cf418d0f26ffefa80cbda5d
ishaongithub <75757332+ishaongithub@users.noreply.github.com>
2021-12-19 10:50:26 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -13,6 +13,12 @@ function setup() {
   canvas.center();
   video.hide();
 }
+function draw(){
+  image(video, 0,0,480,380);
+  if(status!=""){
+    objectDetector.detect(video, gotResult);
+  }
+}
 
 function start()
 {
@@ -26,4 +32,22 @@ function modelLoaded() {
   video.loop();
   video.speed(1);
   video.volume(0);
+}
+function gotResult(error, results){
+  if(error){
+    console.error(error);
+  }
+  console.log(results);
+  objects= results;
+  for(i=0; i<objects.length; i++){
+    document.getElementById("status").innerHTML="Status: Objects Detected";
+    document.getElementById("number_of_objects").innerHTML="Number of objects detected: "+objects.length;
+
+    fill('#FE2020');
+    percent= floor(objects[i].confidence*100);
+    text(objects[i].label + " "+ percent + "%", objects[i].x+15, objects[i].y + 15);
+    noFill();
+    stroke('#010F2F');
+    rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+  }
 }
